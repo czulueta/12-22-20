5321ba52a01a8e19cc747c8abf1a3e7c199cf14b
czulueta <https://github.com/czulueta/weed.git>
2020-12-23 00:08:12 -0800

    message added a route and middleware

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const morgan = require("morgan")
 app.use(express.json())
 app.use(morgan("dev"))
 
+app.use("/cars", require("./routes/carRouter.js"))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({errMsg: err.message})
+})
+
 app.listen(9000, () => {
     console.log("successfully running on port 9000")
 })
